5c964cc534c1643b18d19422866013692d42d403
Gustavo Marques <gtooliveiramarques@gmail.com>
2022-05-27 01:14:59 -0300

    page login

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Login from "./pages/login";
+import Home from "./pages/home";
+
+export type Ipages = {
+  type: "login" | "home";
+};
 
 function App() {
+  const [page, setPage] = useState<Ipages>({ type: "login" });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {page.type === "login" && (
+        <Login setPage={(page: Ipages) => setPage(page)} />
+      )}
+      {page.type === "home" && <Home />}
+    </>
   );
 }
 
---
+++ src/contants/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  background: "#2e3440",
+  backgroundcard: "#3b4252",
+  text: "#eceff4",
+  primary: "#5e81ac",
+  secondary: "#b48ead",
+  success: "#a3be8c",
+  error: "#bf616a",
+};
